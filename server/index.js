63f545f036af1a78d439243f2a3be675a19ac604
Bella Oyedele <delebella@yahoo.com>
2019-08-22 23:43:43 +0100

    configure mongodb

--- server/index.js
+++ server/index.js
@@ -1,27 +1,14 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-const app = express();
 dotenv.config();
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.googleClientId,
-  clientSecret: process.env.googleClientSecret,
-  callbackURL: '/auth/google/callback'
-},
-(accessToken, refreshToken, profile) => {
-  console.log('access token', accessToken);
-  console.log('refresh token', refreshToken);
-  console.log('profile:', profile);
-}));
+mongoose.connect(process.env.mongoURI, { useNewUrlParser: true }); 
 
-app.get('/auth/google', passport.authenticate('google', {
-  scope: ['profile', 'email']
-}));
+const app = express();
 
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there'});
