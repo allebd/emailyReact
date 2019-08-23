d2a3c4be2802265fea7c58b8d60f4153f980aeac
Bella Oyedele <delebella@yahoo.com>
2019-08-23 14:49:34 +0100

    encoding user

--- server/models/User.js
+++ server/models/User.js
@@ -4,3 +4,5 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   googleId: String
 });
+
+mongoose.model('users', userSchema);
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,15 +1,30 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
 
 passport.use(new GoogleStrategy({
   clientID: process.env.googleClientId,
   clientSecret: process.env.googleClientSecret,
   callbackURL: '/auth/google/callback'
 },
-(accessToken, refreshToken, profile) => {
-  console.log('access token', accessToken);
-  console.log('refresh token', refreshToken);
-  console.log('profile:', profile);
+(accessToken, refreshToken, profile, done) => {
+  User.findOne({ googleId: profile.id })
+    .then((existingUser) => {
+      if (existingUser) {
+        // we already have a record with the given profile ID
+        done(null, existingUser);
+      } else {
+        // we don't have a user record with this ID, make a new record
+        new User({ googleId: profile.id }).save()
+          .then(user => done(null, user));
+      }
+    });
 }));
